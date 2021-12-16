5fdca7e9db52893068630fc6a041ff09fb50d778
jaylordbubucayu <57398270+jaylordbubucayu@users.noreply.github.com>
2021-12-16 15:55:08 +0800

    final commit

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,10 +10,10 @@ const Projects = ()=>{
                           
                        
                        
-                        <Card _img={'/images/project_1.jpg'} cardf_title="City Online Market" link={"https://city-market.online/"} />
-                        <Card  _img={'/images/project_2.jpg'} cardf_title="Tracking the Crown" link={"http://trackingthecrown-app.herokuapp.com/"}/>
-                        <Card  _img={'/images/project_3.jpg'} cardf_title="Visual Drums" link={"https://vdrum.herokuapp.com/"}/>
-                        <Card  _img={'/images/project_4.jpg'} cardf_title="Key Code Detector" link={"https://keycode-detector.herokuapp.com/"}/>
+                        <Card _img={'/images/projectcom.jpg'} cardf_title="City Online Market" link={"https://city-market.online/"} />
+                        <Card  _img={'/images/projectcovid.jpg'} cardf_title="Tracking the Crown" link={"http://trackingthecrown-app.herokuapp.com/"}/>
+                        <Card  _img={'/images/projectdrums.jpg'} cardf_title="Visual Drums" link={"https://vdrum.herokuapp.com/"}/>
+                        <Card  _img={'/images/projectkey.jpg'} cardf_title="Key Code Detector" link={"https://keycode-detector.herokuapp.com/"}/>
 
                         </div>
                     
